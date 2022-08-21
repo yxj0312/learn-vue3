f85997c2a972557ba3abfa860fa29585794f308e
yxj0312 <yanxiajun@hotmail.com>
2022-08-21 22:38:25 +0200

    add styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
 <template>
     <main>
         <form action="">
-            <textarea name="" id="" cols="30" rows="10">Hi there</textarea>
+            <textarea name="" id="" cols="30" rows="10" style="width: 100%; height: 300px">Hi there</textarea>
         </form>
     </main>
 </template>
