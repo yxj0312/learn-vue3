5c68594f3106cb203769149a53debbc0af451d21
yxj0312 <yanxiajun@hotmail.com>
2022-08-22 22:34:00 +0200

    onMounted ref value

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,36 @@
 <script setup>
-    textarea.addEventListener("keydown", (e) => {
-        if (e.keyCode === 9) {
-            // tab was pressed
-            // get caret position/selection
-            let val = textarea.value,
-            start = textarea.selectionStart,
-            end = textarea.selectionEnd;
+    import { onMounted, ref } from "vue"
 
-            // set textarea value to: text before cret + tab +text after caret
-            textarea.value = val.substring(0, start) + "\t" + val.substring(end)
+    let textarea = ref('null');
 
-            // put caret at right position again
-            textarea.selectionStart = textarea.selectionEnd = start + 1
-
-            e.preventDefault();s
-        }
+    onMounted(()=>{
+        console.log(textarea.value)
     })
+
+    console.log(textarea.value)
+
+    // textarea.addEventListener("keydown", (e) => {
+    //     if (e.keyCode === 9) {
+    //         // tab was pressed
+    //         // get caret position/selection
+    //         let val = textarea.value,
+    //         start = textarea.selectionStart,
+    //         end = textarea.selectionEnd;
+
+    //         // set textarea value to: text before cret + tab +text after caret
+    //         textarea.value = val.substring(0, start) + "\t" + val.substring(end)
+
+    //         // put caret at right position again
+    //         textarea.selectionStart = textarea.selectionEnd = start + 1
+
+    //         e.preventDefault();
+    //     }
+    // })
 </script>
 <template>
     <main>
         <form>
-            <textarea style="width: 100%; height: 300px">Hi there</textarea>
+            <textarea ref="textarea" style="width: 100%; height: 300px">Hi there</textarea>
         </form>
     </main>
 </template>
