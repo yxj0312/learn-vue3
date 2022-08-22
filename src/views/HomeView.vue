8101e9c22b1a80e4c2a979ec5658d8e1fae23184
yxj0312 <yanxiajun@hotmail.com>
2022-08-22 21:06:58 +0200

    remove unused

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <script setup></script>
 <template>
     <main>
-        <form action="">
-            <textarea name="" id="" cols="30" rows="10" style="width: 100%; height: 300px">Hi there</textarea>
+        <form>
+            <textarea style="width: 100%; height: 300px">Hi there</textarea>
         </form>
     </main>
 </template>
