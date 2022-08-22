6660481116a8208e1f8e1195c84fb6f6fd962707
yxj0312 <yanxiajun@hotmail.com>
2022-08-22 21:11:07 +0200

    add more details

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,22 @@
-<script setup></script>
+<script setup>
+    textarea.addEventListener("keydown", (e) => {
+        if (e.keyCode === 9) {
+            // tab was pressed
+            // get caret position/selection
+            let val = textarea.value,
+            start = textarea.selectionStart,
+            end = textarea.selectionEnd;
+
+            // set textarea value to: text before cret + tab +text after caret
+            textarea.value = val.substring(0, start) + "\t" + val.substring(end)
+
+            // put caret at right position again
+            textarea.selectionStart = textarea.selectionEnd = start + 1
+
+            e.preventDefault();s
+        }
+    })
+</script>
 <template>
     <main>
         <form>
