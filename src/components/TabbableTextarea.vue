e7ec51c7a72b59bf95abd7eeaadd0a0f071a4834
yxj0312 <yanxiajun@hotmail.com>
2022-08-25 22:34:47 +0200

    extract textarea component

---
+++ src/components/TabbableTextarea.vue
@@ -0,0 +1,21 @@
+<template>
+    <textarea @keydown.tab.prevent="onTabPress"></textarea>
+</template>
+
+<script setup>
+    function onTabPress(e) {
+        let textarea = e.target
+        
+        // tab was pressed
+        // get caret position/selection
+        let val = textarea.value,
+        start = textarea.selectionStart,
+        end = textarea.selectionEnd;
+
+        // set textarea value to: text before caret + tab +text after caret
+        textarea.value = val.substring(0, start) + "\t" + val.substring(end)
+
+        // put caret at right position again
+        textarea.selectionStart = textarea.selectionEnd = start + 1
+    }
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,10 @@
 <script setup>
-    import { onMounted, ref } from "vue"
-
-    let textarea = ref('null');
-
-
-    function onTabPress(e) {
-        let textarea = e.target
-        
-        // tab was pressed
-        // get caret position/selection
-        let val = textarea.value,
-        start = textarea.selectionStart,
-        end = textarea.selectionEnd;
-
-        // set textarea value to: text before caret + tab +text after caret
-        textarea.value = val.substring(0, start) + "\t" + val.substring(end)
-
-        // put caret at right position again
-        textarea.selectionStart = textarea.selectionEnd = start + 1
-    }
-
-    
+    import TabbableTextarea from '../components/TabbableTextarea.vue';
 </script>
 <template>
     <main>
         <form>
-            <textarea @keydown.tab.prevent="onTabPress" style="width: 100%; height: 300px">Hi there</textarea>
+            <TabbableTextarea style="width:100%; height:300px" />
         </form>
     </main>
 </template>
